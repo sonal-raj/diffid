a9ea3340b7fbe9a6481cc17198c9891e6321c19b
Sonal Raj <sonal.nitjsr@gmail.com>
2018-08-22 01:45:56 +0530

    Create source template
    Author: Sonal Raj

--- src/diffid.py
+++ src/diffid.py
@@ -1,2 +1,10 @@
 # Base python script for multiplexing the 
 # various diff categories.
+
+import sys
+
+# Main method to parse terminal command and decipher 
+# diff type to invoke relevant utility.
+class Diffid:
+    def main():
+        # Parse args
